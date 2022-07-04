a3af6d51a94914a73c4e14a3c7155fb64caf644c
Alan Shaw <alan.shaw@protocol.ai>
2022-07-04 22:48:09 +0100

    fix: types

--- index.d.ts
+++ index.d.ts
@@ -1,12 +1,29 @@
-import { Transport } from '@libp2p/interface-transport'
+import { Transport, Listener, symbol, CreateListenerOptions, DialOptions } from '@libp2p/interface-transport'
+import { Connection } from '@libp2p/interface-connection'
 import { Multiaddr } from '@multiformats/multiaddr'
 import { WebSocketListener } from './listener'
 
 export { WebSocketListener }
 
-export interface WebSockets extends Transport {
+export declare class WebSockets implements Transport {
+  get [Symbol.toStringTag](): string
+  get [symbol](): true
   /**
-   * Find the listener for the passed multiaddr.
+   * Opens a connection to the passed address.
+   */
+  dial(ma: Multiaddr, options: DialOptions): Promise<Connection>
+  /**
+   * Creates a WebSockets listener. The provided `handler` function will be
+   * called anytime a new incoming Connection has been successfully upgraded
+   * via `upgrader.upgradeInbound`
+   */
+  createListener(options: CreateListenerOptions): Listener
+  /**
+   * Takes a list of `Multiaddr`s and returns only valid Websockets addresses.
+   */
+  filter(multiaddrs: Multiaddr[]): Multiaddr[]
+  /**
+   * Finds the listener for the passed multiaddr.
    */
   listenerForMultiaddr (a: Multiaddr): WebSocketListener | undefined
 }
